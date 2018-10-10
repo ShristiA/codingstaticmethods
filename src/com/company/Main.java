9f47c6ae96b4aa85f303d2ee5607eb309c92304c
ShristiA <shristiamatya3@gmail.com>
2018-10-10 06:07:22 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+//        private|public static returntype methodName([parameter list]){
+//            ...statements
+//        }
+
+
+
+
+//        A static method without the return type and parameter
+//                private static void printWelcomeMessage(){
+//            System.out.println("Hello Mew user!");
+//
+//        }
+
+
+//        A static method with three parameters that returns a double value
+//                public static double calculatevalue(double monthlyinv, double monthlyinterestrate; int months){
+//            double futurevalue = 0.0;
+//            for(int i =1; i<=months; i++){
+//                futureValue = (futureValue+monthlyinv) *(1+monthlyinterestRate);
+//            }
+//            return futureValue
+//        }
+
+
+//        Calling methods that is in the same class - Syntax
+//   methodsname([argument list]);
+//        
+//        printWelcomeMeaage();
+//        calculatevalue(double monthlyinv, double rate; int months);
+
+    }
+}
